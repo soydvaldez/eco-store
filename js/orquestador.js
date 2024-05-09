4203a59ce88da9184dd11fd730dede9077e63b2a
dvaldez <dvaldez@no-reply.com>
2024-05-08 18:36:16 -0600

    TA 00010 pantalla - index.html   JS(orquestador.js)   -Se agrego un loader de carga principal para la pantalla. Es bloqueante en la pantalla  y desactiva el scroll del documento.

---
+++ js/orquestador.js
@@ -0,0 +1,47 @@
+let $body = document.body;
+
+function createLoader() {
+  let $loader = document.createElement("div");
+  $loader.innerHTML = `<div class="main-loader hidden">
+           <div class="loader-properties loader-animation"></div>
+           <span>Cargando...</span>
+       </div>`;
+  $body.appendChild($loader.childNodes[0]);
+
+  let event = new Event("created-loader", {
+    bubbles: true,
+    composed: true,
+  });
+
+  document.body.dispatchEvent(event);
+}
+
+function removeMainLoader() {
+  let isExistLoader = $body.querySelector(".main-loader.hidden");
+
+  if (isExistLoader) {
+    isExistLoader.remove();
+  }
+}
+
+function handleScrolling() {
+  document.body.classList.toggle("disable-scroll");
+}
+
+function displayMainLoader() {
+  let $mainloader = document.querySelector(".main-loader");
+  $mainloader.classList.toggle("hidden");
+}
+
+function ctrlLoader() {
+  displayMainLoader();
+  handleScrolling();
+}
+
+createLoader();
+ctrlLoader();
+
+setTimeout((e) => {
+  ctrlLoader();
+  // removeMainLoader();
+}, 1000);
